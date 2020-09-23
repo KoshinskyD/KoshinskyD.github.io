2fa04d1f77846b84d9ef4c2c6496d06e923aabcb
KoshinskyD <71724376+KoshinskyD@users.noreply.github.com>
2020-09-23 11:38:34 -0600

    template

--- template/sketch.js
+++ template/sketch.js
@@ -5,23 +5,10 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let shouldIShowTheEllipse = false;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  
-  let something = ture;
-
-  if(shouldIShowTheEllipse) {
-    fill('red')
-    ellipse(mouseX, mouseY, 100, 100);
-  }
-}
-
-function mousePressed() {
-  shouldIShowTheEllipse = !shouldIShowTheEllipse;
 }
