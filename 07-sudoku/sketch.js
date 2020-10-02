54ba063209df880a05cdb3682043ad9b98330c69
KoshinskyD <71724376+KoshinskyD@users.noreply.github.com>
2020-10-02 09:47:04 -0600

    Sudoku

--- 07-sudoku/sketch.js
+++ 07-sudoku/sketch.js
@@ -1,54 +1,53 @@
-// Project Title
+// Sudoku 2d array demo
 
-const sudokuSize = 9;
+let sudoku;
+let initialState;
+
+const GRIDSIZE = 9;
 let cellSize;
-let centerX, centerY;
 
-let sudoku = [[0,0,9,0,0,2,0,0,5],
-              [5,3,8,0,6,4,0,0,9],
-              [1,6,2,0,0,0,0,3,0],
-              [0,0,3,0,2,7,0,0,0],
-              [0,5,4,6,0,0,1,0,0],
-              [0,0,7,0,1,5,3,4,0],
-              [3,0,0,8,0,1,9,0,6],
-              [7,0,0,3,0,0,8,5,0],
-              [0,9,1,0,0,0,4,7,0]];
-
-let initialState = [[0,0,9,0,0,2,0,0,5],
-                    [5,3,8,0,6,4,0,0,9],
-                    [1,6,2,0,0,0,0,3,0],
-                    [0,0,3,0,2,7,0,0,0],
-                    [0,5,4,6,0,0,1,0,0],
-                    [0,0,7,0,1,5,3,4,0],
-                    [3,0,0,8,0,1,9,0,6],
-                    [7,0,0,3,0,0,8,5,0],
-                    [0,9,1,0,0,0,4,7,0]];
-
+function preload() {
+  sudoku = loadStrings("assets/1.txt");
+  initialState = loadStrings("assets/1.txt");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  if (height < width) {
-    cellSize = height/sudokuSize;
-    centerX = (width - cellSize*sudokuSize) / 2;
-    centerY = 0;
+
+  // convert suduko into 2d array
+  for (let i=0; i<sudoku.length; i++) {
+    sudoku[i] = sudoku[i].split(",");
+    initialState[i] = initialState[i].split(",");
+  }
+
+  //loop through the whole 2d array, and turn everything to numbers
+  for (let y=0; y<GRIDSIZE; y++) {
+    for (let x=0; x<GRIDSIZE; x++) {
+      sudoku[y][x] = int(sudoku[y][x]);
+      initialState[y][x] = int(initialState[y][x]);
+    }
+  }
+
+  if (width < height) {
+    cellSize = width / GRIDSIZE;
   }
   else {
-    cellSize = width/sudokuSize; 
-    centerY = (height - cellSize*sudokuSize) / 2;
-    centerX = 0;
+    cellSize = height / GRIDSIZE;
   }
 }
 
 function draw() {
   background(220);
-  dissplaySudoku();
+  displayGrid();
 }
 
-function dissplaySudoku() {
-  for (let y = 0; y < sudokuSize; y++) {
-    for (let x = 0; x < sudokuSize; x++) {
+function displayGrid() {
+  //squares and numbers
+  for (let y=0; y<GRIDSIZE; y++) {
+    for (let x=0; x<GRIDSIZE; x++) {
+      strokeWeight(1);
       fill("white");
-      rect(x* cellSize + centerX, y*cellSize + centerY, cellSize, cellSize);
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
 
       if (sudoku[y][x] !== 0) {
         //show number
@@ -60,41 +59,33 @@ function dissplaySudoku() {
         else {
           fill("black");
         }
-
-        textSize(cellSize * 0.7);
+        textSize(cellSize * 0.8);
         textAlign(CENTER, CENTER);
 
-        text(sudoku[y][x], x*cellSize + centerX + cellSize/2, y*cellSize + centerY + cellSize/2);
-       
+        text(sudoku[y][x], x*cellSize + cellSize/2, y*cellSize + cellSize/2);
       }
     }
   }
 
-  push();
+  //mini-grid lines
   strokeWeight(5);
-  // thick lines deviding in 9ths
-  line(cellSize*3 + centerX, 0 + centerY, cellSize*3 + centerX, cellSize*sudokuSize  + centerY);
-  line(cellSize*6 + centerX, 0 + centerY, cellSize*6 + centerX, cellSize*sudokuSize + centerY);
-  line(0 + centerX, cellSize*3 + centerY, cellSize*sudokuSize + centerX, cellSize*3 + centerY);
-  line(0 + centerX, cellSize*6 + centerY, cellSize*sudokuSize + centerX, cellSize*6 + centerY);
-  
-  // outline
-  line(0 + centerX, 0 + centerY, sudokuSize*cellSize + centerX, 0 + centerY); // top
-  line(0 + centerX , 0 + sudokuSize*cellSize + centerY, sudokuSize*cellSize + centerX, 0 + sudokuSize*cellSize + centerY); // bottom
-  line(0 + centerX, 0 + centerY, 0 + centerX, sudokuSize*cellSize + centerY); // left
-  line(0 + sudokuSize*cellSize + centerX, 0 + centerY, 0 + sudokuSize*cellSize + centerX, 0 + sudokuSize*cellSize + centerY); // right
-  pop();
+  line(0, 3*cellSize, 9*cellSize, 3*cellSize);
+  line(0, 6*cellSize, 9*cellSize, 6*cellSize);
+  line(3*cellSize, 0, 3*cellSize, 9*cellSize);
+  line(6*cellSize, 0, 6*cellSize, 9*cellSize);
 }
 
-function changecell(cellX, cellY){
-  if (sudoku[cellY][cellX] !== initialState[cellY][cellX] || sudoku[cellY][cellX] === 0) {
-    sudoku[cellY][cellX] = key;
-  }
-}
 
 function mousePressed() {
   let cellX = floor(mouseX / cellSize);
   let cellY = floor(mouseY / cellSize);
 
-  changecell(cellX, cellY); 
+  changeCell(cellX, cellY);
+}
+
+function changeCell(x, y) {
+  if (sudoku[y][x] !== initialState[y][x] || sudoku[y][x] === 0) {
+    //don't go into double digits
+    sudoku[y][x] = (sudoku[y][x] + 1) % 10;
+  }
 }
